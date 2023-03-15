52f48f9d81356f3412231adf0eecd67041c4f25c
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-15 16:38:23 +0300

    Signup page edited

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,71 +1,82 @@
 import './Signup.module.css'
 
-
-import {Container, Typography, Button, FormControl, OutlinedInput, InputLabel, InputAdornment, IconButton} from '@mui/material'
+import { useSignup } from '../../hooks/useSignup'
+import { Container, Typography, Button, FormControl, OutlinedInput, InputLabel, InputAdornment, IconButton } from '@mui/material'
 import { useState } from 'react'
-import {Visibility, VisibilityOff} from '@mui/icons-material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
 
 export default function Signup() {
 
-    const [values,setValues]= useState({
-        email:'',
-        password:'',
-        showPassword:false,
-        userName:''
+  const { signup, hata, bekliyor } = useSignup();
+
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    showPassword: false,
+    userName: ''
+  })
+
+  //prop ile hangi form kontrol değiştiğini bulacak
+  //event ile değişen veriyi bulacak
+  const handleChange = (prop) => (event) => {
+    setValues({
+      ...values, [prop]: event.target.value
     })
+  }
 
-    //prop ile hangi form kontrol değiştiğini bulacak
-    //event ile değişen veriyi bulacak
-    const handleChange=(prop)=>(event)=>{
-            setValues({
-                ...values,[prop]: event.target.value
-            })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(values);
+    signup(values.email, values.password, values.userName);
+  }
 
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        console.log(values);
-    }
-
-    const handleClickShowPassword=()=>{
-        setValues({
-            ...values,showPassword: !values.showPassword
-        })
-    }
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values, showPassword: !values.showPassword
+    })
+  }
 
   return (
     <Container>
-        <form onSubmit={handleSubmit}>
-            <Typography sx={{mt:15, ml:5, fontWeight:'bold'}} variant="h4" color="darkslateblue">
-                ÜYE OL
-            </Typography>
+      <form onSubmit={handleSubmit}>
+        <Typography sx={{ mt: 15, ml: 5, fontWeight: 'bold' }} variant="h4" color="darkslateblue">
+          ÜYE OL
+        </Typography>
 
-            <FormControl fullWidth sx={{mt:5}}>
-                <InputLabel htmlFor="email">Email</InputLabel>
-                <OutlinedInput id="email" label="Email" value={values.email} onChange={handleChange('email')}/>
-            </FormControl>
+        <FormControl fullWidth sx={{ mt: 5 }}>
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <OutlinedInput id="email" label="Email" value={values.email} onChange={handleChange('email')} />
+        </FormControl>
 
-            <FormControl fullWidth sx={{mt:5}}>
-                <InputLabel htmlFor="email">Parola</InputLabel>
-                <OutlinedInput type={values.showPassword ? 'text': 'password'} id="parola" label="Parola" value={values.password} onChange={handleChange('password')} endAdornment={
-                    <InputAdornment position='end'>
-                        <IconButton aria-label='Toggle password' onClick={handleClickShowPassword} edge="end">
-                            {values.showPassword ? <VisibilityOff/>: <Visibility/> }
-                        </IconButton>
-                    </InputAdornment>
-                }/>
-            </FormControl>
+        <FormControl fullWidth sx={{ mt: 5 }}>
+          <InputLabel htmlFor="email">Parola</InputLabel>
+          <OutlinedInput type={values.showPassword ? 'text' : 'password'} id="parola" label="Parola" value={values.password} onChange={handleChange('password')} endAdornment={
+            <InputAdornment position='end'>
+              <IconButton aria-label='Toggle password' onClick={handleClickShowPassword} edge="end">
+                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          } />
+        </FormControl>
 
-            <FormControl fullWidth sx={{my:5}}>
-                <InputLabel htmlFor="user-name">Kullanıcı ad</InputLabel>
-                <OutlinedInput id="user-name" label="Kullanıcı ad" value={values.userName} onChange={handleChange('userName')}/>
-            </FormControl>
+        <FormControl fullWidth sx={{ my: 5 }}>
+          <InputLabel htmlFor="user-name">Kullanıcı ad</InputLabel>
+          <OutlinedInput id="user-name" label="Kullanıcı ad" value={values.userName} onChange={handleChange('userName')} />
+        </FormControl>
 
+        {!bekliyor &&
+          <Button variant='contained' type='submit' color='info' size='large' sx={{ mt: 5 }}>
+            ÜYE OL
+          </Button>
+        }
+        {bekliyor &&
+          <Button disabled variant='contained' type='submit' color='info' size='large' sx={{ mt: 5 }}>
+            BEKLİYOR
+          </Button>
+        }
+        {hata && <p>{hata}</p>}
 
-            <Button variant='contained' type='submit' color='info' size='large' sx={{mt:5}}>
-                ÜYE OL
-            </Button>
-        </form>
+      </form>
     </Container>
   )
 }
