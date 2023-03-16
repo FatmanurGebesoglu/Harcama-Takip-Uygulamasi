d7e1ce6046ea7f30a50ffa915dcb365a0850e0b7
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-16 11:44:06 +0300

    Home page edited

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,20 @@
 import './Home.module.css'
+import {Container, Grid,Paper} from '@mui/material'
+import Form from './Form'
+
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <Container sx={{mt:8}}>
+        <Grid container spacing={2}>
+            <Grid item md={8} sm={12} xs={12} >
+                  <Paper>Liste</Paper>
+            </Grid>
+            <Grid item md={4} sm={12} xs={12} >
+                  <Form/>
+            </Grid>
+        </Grid>
+    </Container>
   )
 }
 
